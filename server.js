3a57a9e75e2b689d1ff76ed25ef176cbcf926b6a
Chris Stroupe <cstroupe79@gmail.com>
2019-07-04 09:00:02 -0500

    working page

--- server.js
+++ server.js
@@ -93,19 +93,19 @@ app.get("/scrape", function (req, res) {
     // res.send("Scrape Complete");
   });
   // function render(data){
-  //   res.render("index", {data})
-  // };
-});
-
-
-
-// Route for getting all Articles from the db
-app.get("/articles", function (req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
+    // };
+  });
+  
+  
+  
+  // Route for getting all Articles from the db
+  app.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
     .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      res.render("index", {dbArticle})
+      // res.json(dbArticle);
     })
     .catch(function (err) {
       // If an error occurred, send it to the client
